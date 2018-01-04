6986b67280020b800bc1113e5023be37496a762a
aleclarson <alec.stanford.larson@gmail.com>
2018-01-04 12:41:59 -0500

    add 'warn: ' prefix to log.warn calls

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,10 @@
 
 const timber = require('timber')
+const huey = require('huey')
+
+timber.enable('tags', {
+  warn: huey.yellow('warn: ')
+})
 
 exports.log = timber.create(process.env.LOG_LEVEL)
-exports.huey = require('huey')
+exports.huey = huey
